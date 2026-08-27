6293c133fd97cdebb8945944f4e0938bd424c047
lagnuty <lagnuty@gmail.com>
2026-08-27 15:25:06 +0300

    Publish Xray builds as GitHub releases

--- Telegram/SourceFiles/core/update_checker.cpp
+++ Telegram/SourceFiles/core/update_checker.cpp
@@ -1890,7 +1890,7 @@ void UpdateApplication() {
 			} else if (KSandbox::isSnap()) {
 				return "https://snapcraft.io/telegram-desktop";
 			}
-			return "https://desktop.telegram.org";
+			return "https://github.com/Lagnuty/tdesktop-xray/releases/latest";
 #endif // OS_WIN_STORE || OS_MAC_STORE
 		}();
 		UrlClickHandler::Open(url);
